808ce0df15f3a3b1f36db0e20a3025c78dd83e35
R3L4MPAGO <82402826+R3L4MPAGO@users.noreply.github.com>
2025-04-01 19:44:23 +0000

    desafio nivel mestre

--- BatalhaDeCastasDoSuperTrunfo.c
+++ BatalhaDeCastasDoSuperTrunfo.c
@@ -7,26 +7,30 @@ int main(){
     char Codigo_1[5];
 
     char NomeDaCidade_1[20];
-    int Populacao_1;
+    unsigned long int Populacao_1;
     float Area_1;
     float PIB_1;
     int PontosTuristicos_1;
-
+ 
     float DensidadePopulacional_1;
     float PIBperCapita_1;
+
+    float SuperPoder_1;
 
     // VARIÁVEIS DA CARTA 2
     char Estado_2;
     char Codigo_2[5];
 
     char NomeDaCidade_2[20];
-    int Populacao_2;
+    unsigned long int Populacao_2;
     float Area_2;
     float PIB_2;
     int PontosTuristicos_2;
 
     float DensidadePopulacional_2;
     float PIBperCapita_2;
+
+    float SuperPoder_2;
 
     // CARTA 1
     printf("\n===== CARTA 1 =====\n");
@@ -40,7 +44,7 @@ int main(){
     scanf(" %s", NomeDaCidade_1);
         
     printf("Carta 1: Insira o número de habitantes: [int] \n");
-    scanf(" %d", &Populacao_1);
+    scanf(" %lu", &Populacao_1);
         
     printf("Carta 1: Insira a área da cidade em quilômetros quadrados: [float] \n");
     scanf(" %f", &Area_1);
@@ -63,7 +67,7 @@ int main(){
     scanf(" %s", NomeDaCidade_2);
         
     printf("Carta 2: Insira o número de habitantes: [int] \n");
-    scanf(" %d", &Populacao_2);
+    scanf(" %lu", &Populacao_2);
         
     printf("Carta 2: Insira a área da cidade em quilômetros quadrados: [float] \n");
     scanf(" %f", &Area_2);
@@ -80,8 +84,10 @@ int main(){
     PIBperCapita_1 = PIB_1 / Populacao_1;
     PIBperCapita_2 = PIB_2 / Populacao_2;
 
-    printf("\n===== CARTA 1 ===== \nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f\nNúmero de Pontos Turísticos: %d\nDensidade Populacional: %.2f\nPIB per Capita: %.2f\n", Estado_1, Codigo_1, NomeDaCidade_1, Populacao_1, Area_1, PIB_1, PontosTuristicos_1, DensidadePopulacional_1, PIBperCapita_1);
-    printf("\n===== CARTA 2 ===== \nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f\nNúmero de Pontos Turísticos: %d\nDensidade Populacional: %.2f\nPIB per Capita: %.2f", Estado_2, Codigo_2, NomeDaCidade_2, Populacao_2, Area_2, PIB_2, PontosTuristicos_2, DensidadePopulacional_2, PIBperCapita_2);
+    SuperPoder_1 = Populacao_1 + Area_1 + PIB_1 + PontosTuristicos_1 + PIBperCapita_1 + (1 / DensidadePopulacional_1);
+    SuperPoder_2 = Populacao_2 + Area_2 + PIB_2 + PontosTuristicos_2 + PIBperCapita_1 + (1 / DensidadePopulacional_2);
+
+    printf("\n===== Comparação de Cartas ===== \n Carta 1 = 1\n Carta 2 = 0\n\nPopulação: (%d) venceu!\nÁrea: (%d) venceu!\nPIB: (%d) venceu!\nPontos Turísticos: (%d) venceu!\nDensidade Populacional: (%d) venceu!\nPIB per Capita: (%d) venceu!\nSuper Poder: (%d) venceu!", Populacao_1 > Populacao_2, Area_1 > Area_2, PIB_1 > PIB_2, PontosTuristicos_1 > PontosTuristicos_2, DensidadePopulacional_1 < DensidadePopulacional_2, PIBperCapita_1 > PIBperCapita_2, SuperPoder_1 > SuperPoder_2);
 
     return 0;
 }
